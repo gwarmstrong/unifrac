9af6f3e104063078514ab0aeaaf324db90fc6df6
gwarmstrong <garmstrong@colgate.edu>
2019-07-17 18:02:54 -0700

    additional test case

--- unifrac/tests/test_longitudinal.py
+++ unifrac/tests/test_longitudinal.py
@@ -27,13 +27,20 @@ class EMDUnifracTests(unittest.TestCase):
                                                         ids,
                                                         sample_1,
                                                         sample_2)
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+
         # answer from paper
         expected = 0.2333333
 
         self.assertAlmostEqual(expected, distance)
-        # TODO check diff_abund
 
     def test_weighted_emd_unifrac_root_diff(self):
+        # TODO check with skbio
         tree_str = '((1:0.2,2:0.1)5:0.3,(3:0.1,4:0.2)6:0.3)root;'
         tree = TreeNode.read(StringIO(tree_str))
         tree.assign_ids()
@@ -46,7 +53,41 @@ class EMDUnifracTests(unittest.TestCase):
                                                         sample_1,
                                                         sample_2)
 
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        print(id_mapping)
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+        # print(diff_abund)
         self.assertAlmostEqual(0.45, distance)
+
+    def test_weighted_emd_unifrac_bigger_example(self):
+        tree_str = '(((1:0.3, 2:0.3)6:0.5,(3:0.1,4:0.1)7:0.1)8:0.1,5:0.1)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['1', '2', '3', '4', '5']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([0, 0, 1/4, 1/4, 1/2])
+        sample_2 = np.array([1/4, 1/4, 1/4, 1/4, 0])
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        print(id_mapping)
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+        # print(diff_abund)
+        # multiply by 4 to get counts
+        expected = beta.weighted_unifrac(4 * sample_1, 4 * sample_2, names,
+                                         tree)
+        self.assertAlmostEqual(expected, distance)
 
     def test_unweighted_emd_unifrac_from_author_implementation(self):
         # tree_str = '((B:0.1,C:0.2)A:0.3)root;' # author's tree
@@ -65,7 +106,11 @@ class EMDUnifracTests(unittest.TestCase):
 
         distance = distance / tree.descending_branch_length()
 
-        print('expected: {}'.format(expected))
+        id_mapping = {tree.find_by_id(id_).id: tree.find_by_id(id_).name for
+                      id_ in ids}
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+        print(diff_abund_names)
         self.assertAlmostEqual(expected, distance)
 
     def test_unweighted_emd_unifrac_from_paper(self):
@@ -85,11 +130,12 @@ class EMDUnifracTests(unittest.TestCase):
         distance = distance / tree.descending_branch_length()
 
         print(diff_abund)
-        print({id_: tree.find_by_id(id_) for id_ in ids})
+        print({tree.find_by_id(id_).name: tree.find_by_id(id_) for id_ in ids})
         print('expected: {}'.format(expected))
         self.assertAlmostEqual(expected, distance)
         # TODO check diff_abund
 
+# TODO check
 
 if __name__ == "__main__":
     unittest.main()
