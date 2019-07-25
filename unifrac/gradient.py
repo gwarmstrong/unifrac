557c6b692c1e4e8cd25e2cd698e92efd99ab218c
gwarmstrong <garmstrong@colgate.edu>
2019-07-25 13:50:54 -0700

    rework some outputs

---
+++ unifrac/gradient.py
@@ -0,0 +1,263 @@
+from typing import Union, List, Tuple, Dict
+import biom
+import numpy as np
+import skbio
+from biom import load_table
+
+available_metrics = {'weighted_unifrac', 'unweighted_unifrac'}
+
+
+def hotspot_pairs(table: Union[str, biom.Table],
+                  tree: Union[str, skbio.TreeNode],
+                  pairs: List[Tuple[str, str]],
+                  metric: str = 'weighted_unifrac'):
+    """
+
+    Parameters
+    ----------
+    table
+        An instance or filepath to a BIOM 2.1 formatted table (HDF5)
+    tree
+        A skbio.TreeNode or filepath to a Newick formatted tree
+    pairs
+        A list of pairs of Sample ID's in the given `table`
+        corresponding to pairs that should be compared to generate
+        hotspots
+    metric
+        The pairwise distance function to apply.
+        Options: `weighted_unifrac` and `unweighted_unifrac`
+
+    Returns
+    -------
+        Dictionary containing a dictionary of node statistics for the
+        `hotspot` of each pair in `pairs`
+
+    Raises
+    ------
+    # TODO
+
+    Notes
+    -----
+    # TODO
+
+    References
+    ----------
+    # TODO
+
+    Examples
+    --------
+    >>> from unifrac.gradient import hotspot_pairs
+    >>> pairs = [('A', 'B'), ('B', 'C')]
+    >>> hotspot_pairs('e1.biom', 't1.newick', pairs, metric='weighted_unifrac')
+
+    """
+    if isinstance(tree, str):
+        tree = skbio.TreeNode.read(tree)
+    elif not isinstance(tree, skbio.TreeNode):
+        raise ValueError("Unsupported type {} for tree.".format(
+            type(tree)))
+
+    if isinstance(table, str):
+        table = load_table(table)
+    elif not isinstance(table, biom.Table):
+        raise ValueError("Unsupported type {} for table.".format(
+            type(table)))
+
+    if metric not in available_metrics:
+        raise ValueError("Unsupported metric '{}'".format(metric))
+
+    # TODO assert all ids in table are in tree, might be able to reuse
+    #  validation functions from skbio
+
+    tree.assign_ids()
+
+    observation_names = table.ids('observation')
+
+    all_results = dict()
+    for i, (sample_1, sample_2) in enumerate(pairs):
+        sample_1_data = table.data(sample_1)
+        sample_2_data = table.data(sample_2)
+        all_results[(sample_1, sample_2)] = hotspot(sample_1_data,
+                                                    sample_2_data,
+                                                    observation_names,
+                                                    tree,
+                                                    metric=metric)
+    max_profiles = [result[0] for result in all_results]
+    differential_abundances = [result[1] for result in all_results]
+    return all_results  # max_profiles, differential_abundances
+
+
+def hotspot(u_counts: Union[np.array, np.array],
+            v_counts: Union[List, np.array],
+            otu_ids: Union[List, np.array],
+            tree: Union[str, skbio.TreeNode],
+            metric='weighted_unifrac') -> Dict:
+    """
+
+    Parameters
+    ----------
+    u_counts, v_counts
+        Vectors of counts/relative abundances of OTUs for two samples. Must be
+        equal length.
+    otu_ids
+        Vector of OTU ids corresponding to tip names in tree. Must be the
+        same length as u_counts and v_counts.
+    tree
+        Tree relating the OTUs in otu_ids. The set of tip names in the
+        tree can be a superset of otu_ids, but not a subset.
+    metric
+        The pairwise distance function to apply. Options: `weighted_unifrac`
+        and `unweighted_unifrac`
+
+    Returns
+    -------
+        Dictionary of statistics on the node identified as contributing the
+        most to the distance specified by `metric`
+
+    Raises
+    ------
+    # TODO
+
+    Notes
+    -----
+    # TODO
+
+    References
+    ----------
+    # TODO
+
+    Examples
+    --------
+    >>> from unifrac.gradient import hotspot
+    >>> u_counts = [0, 0, 1, 1, 2]
+    >>> v_counts = [1, 1, 1, 1, 0]
+    >>> otu_ids = ['a', 'b', 'c', 'd', 'e']
+    >>> tree = 't1.newick'
+    >>> hotspot(u_counts, v_counts, otu_ids, tree, metric='weighted_unifrac')
+
+    """
+    # basically just takes the max of diff_abund from
+    # _emd_unifrac_single_pair and returns the profile for that hotspot
+    # TODO allow stringIO or file object?
+    if isinstance(tree, str):
+        tree = skbio.TreeNode.read(tree)
+    elif not isinstance(tree, skbio.TreeNode):
+        raise ValueError("Unsupported type {} for tree.".format(type(tree)))
+    tree = tree.shear(otu_ids)
+    max_change_node, differential_abundances = _calculate_hotspot(u_counts,
+                                                                  v_counts,
+                                                                  otu_ids,
+                                                                  tree,
+                                                                  metric)
+    profile = _profile_tree_node(max_change_node)
+    return profile, differential_abundances
+
+
+# TODO with EMDUnifrac there may be some concern over uniqueness of
+#  hotspots, but I am not sure how this will play out on real data
+def _calculate_hotspot(u_counts: Union[np.array, List],
+                       v_counts: Union[np.array, List],
+                       otu_ids: Union[List, np.array],
+                       tree: skbio.TreeNode,
+                       metric='weighted_unifrac') -> skbio.TreeNode:
+    # general idea with EMD unifrac: should be able to adjust weights for
+    # different unifrac methods
+    u_counts = np.array(u_counts)
+    v_counts = np.array(v_counts)
+    u_counts, v_counts = _weight_adjuster(u_counts,
+                                          v_counts,
+                                          tree,
+                                          method=metric)
+
+    # makes sure each node has an id so we can refer to it later
+    # ids are assigned by postorder traversal
+    tree.assign_ids()
+
+    ids = [tree.find(name).id for name in otu_ids]
+
+    _, differential_abundances = _emd_unifrac_single_pair(tree,
+                                                          ids,
+                                                          u_counts,
+                                                          v_counts)
+    # find most extreme differential abundance
+    max_abs_diff_abund = -1
+    # max_change_node_id = tree.find('root')
+    # TODO edge case where samples are exactly same
+    for id_, diff_abund in differential_abundances.items():
+        abs_diff_abund = abs(diff_abund)
+        if abs_diff_abund > max_abs_diff_abund:
+            max_abs_diff_abund = abs_diff_abund
+            max_change_node_id = id_
+
+    max_change_node = tree.find_by_id(max_change_node_id)
+
+    return max_change_node, differential_abundances
+
+
+def _weight_adjuster(sample_1: np.array,
+                     sample_2: np.array,
+                     tree: skbio.TreeNode = None,
+                     method: str = 'weighted_unifrac'):
+    # TODO assumes non-negative data
+    if method == 'unweighted_unifrac':
+        if tree is None:
+            raise ValueError('Unweighted weight adjustment requires a tree.')
+        total_branch_length = tree.descending_branch_length()
+        sample_1 = (sample_1 > 0).astype(float) / total_branch_length
+        sample_2 = (sample_2 > 0).astype(float) / total_branch_length
+    elif method == 'weighted_unifrac':
+        sample_1 = sample_1 / sample_1.sum()
+        sample_2 = sample_2 / sample_2.sum()
+    else:
+        raise ValueError("method: '{}' not recognized.".format(method))
+
+    return sample_1, sample_2
+
+
+def _profile_tree_node(node: skbio.TreeNode) -> dict:
+    # returns dict with {'distance_to_root', 'clade_width',
+    # 'maximally_divergent_tips', 'node_address'}
+    clade_width, maximally_divergent_tips = node.get_max_distance()
+    maximally_divergent_tips = [tip.id if tip is not None else None for tip in
+                                maximally_divergent_tips]
+    output = {'distance_to_root': node.distance(node.root()),
+              'clade_width': clade_width,
+              'maximally_divergent_tips': maximally_divergent_tips,
+              'node_address': node.id}
+    return output
+
+
+# TODO potentially revist signature
+def _emd_unifrac_single_pair(tree: skbio.TreeNode,
+                             ids: list,  # really could be any enumerated type
+                             sample_1_weights: np.array,
+                             sample_2_weights: np.array) -> \
+                             Tuple[float, dict]:
+    # unweighted is present in the source
+    # https://github.com/dkoslicki/EMDUnifrac/blob/master/src/EMDUnifrac.py
+    # EMDUnifrac_unweighted
+    # runs emd_unifrac and returns
+    # returns unifrac score and differential_abundance
+
+    # TODO assumes all nodes have id
+
+    distance = 0
+    differential_abundance = dict()
+
+    partial_sum_array = sample_1_weights - sample_2_weights
+
+    partial_sums = dict(zip(ids, partial_sum_array))
+    for node in tree.postorder(include_self=False):
+        # TODO better variable names
+        node_id = node.id
+        val = partial_sums.pop(node_id, 0)
+        parent_id = node.parent.id
+        parent_val = partial_sums.get(parent_id, 0)
+        parent_val += val
+        partial_sums[parent_id] = parent_val
+        if val != 0:
+            # TODO edge case of identical samples?
+            differential_abundance[node_id] = node.length * val
+        distance += node.length * abs(val)
+
+    return distance, differential_abundance
---
+++ unifrac/tests/test_gradient.py
@@ -0,0 +1,334 @@
+import unittest
+from unittest import mock
+from io import StringIO
+from unittest import TestCase
+import pkg_resources
+
+import numpy as np
+from skbio import TreeNode
+from skbio.diversity import beta
+from biom import load_table
+
+from unifrac.gradient import (_emd_unifrac_single_pair,
+                              _weight_adjuster,
+                              hotspot,
+                              hotspot_pairs)
+
+
+class TestHotspotPairs(unittest.TestCase):
+
+    package = 'unifrac.tests'
+
+    def setUp(self):
+        self.tree_path = self.get_data_path('t1.newick')
+        self.table_path = self.get_data_path('e1.biom')
+
+    def get_data_path(self, filename):
+        # adapted from qiime2.plugin.testing.TestPluginBase
+        return pkg_resources.resource_filename(self.package,
+                                               'data/%s' % filename)
+
+    def test_hotspot_pairs_paths(self):
+        pairs = [('A', 'B'), ('B', 'C')]
+        hotspot_dict = hotspot_pairs(self.table_path,
+                                     self.tree_path,
+                                     pairs,
+                                     metric='unweighted_unifrac')
+        a_b_node_id = 7
+        b_c_node_id = 4
+        self.assertEqual(a_b_node_id, hotspot_dict[('A', 'B')]['node_address'])
+        self.assertEqual(b_c_node_id, hotspot_dict[('B', 'C')]['node_address'])
+
+    def test_hotspot_pairs_objects(self):
+        pairs = [('A', 'B'), ('B', 'C')]
+        table = load_table(self.table_path)
+        tree = TreeNode.read(self.tree_path)
+        hotspot_dict = hotspot_pairs(table,
+                                     tree,
+                                     pairs,
+                                     metric='unweighted_unifrac')
+        a_b_node_id = 7
+        b_c_node_id = 4
+        self.assertEqual(a_b_node_id, hotspot_dict[('A', 'B')]['node_address'])
+        self.assertEqual(b_c_node_id, hotspot_dict[('B', 'C')]['node_address'])
+
+    def test_hotspot_pairs_unsupported_tree_type(self):
+        pairs = [('A', 'B'), ('B', 'C')]
+        with self.assertRaisesRegex(ValueError, r"Unsupported.*tree"):
+            hotspot_pairs(self.table_path,
+                          [],
+                          pairs,
+                          metric='unweighted_unifrac')
+
+    def test_hotspot_pairs_unsupported_table_type(self):
+        pairs = [('A', 'B'), ('B', 'C')]
+        with self.assertRaisesRegex(ValueError, r"Unsupported.*table"):
+            hotspot_pairs([],
+                          self.tree_path,
+                          pairs,
+                          metric='unweighted_unifrac')
+
+    def test_hotspot_pairs_unsupported_table_metric(self):
+        pairs = [('A', 'B'), ('B', 'C')]
+        with self.assertRaisesRegex(ValueError, r"Unsupported metric"):
+            hotspot_pairs(self.table_path,
+                          self.tree_path,
+                          pairs,
+                          metric='not-a-real-metric')
+
+
+class TestHotspot(unittest.TestCase):
+
+    package = 'unifrac.tests'
+
+    def setUp(self):
+        self.tree_str = '(((1:0.3, 2:0.3)6:0.5,(3:0.1,4:0.1)7:0.1)8:0.1,' \
+                    '5:0.1)root;'
+        tree = TreeNode.read(StringIO(self.tree_str))
+        tree.assign_ids()
+        self.tree = tree
+        self.otu_ids = ['1', '2', '3', '4', '5']
+        self.u_counts = [0, 0, 1, 1, 2]
+        self.v_counts = [1, 1, 1, 1, 0]
+
+    def get_data_path(self, filename):
+        # adapted from qiime2.plugin.testing.TestPluginBase
+        return pkg_resources.resource_filename(self.package,
+                                               'data/%s' % filename)
+
+    def test_hotspot_gets_profile(self):
+        with mock.patch('unifrac.gradient._calculate_hotspot') as \
+                mocked_hotspot:
+            mocked_return_node = self.tree
+            mocked_hotspot.return_value = mocked_return_node
+            hotspot_profile = hotspot(self.u_counts,
+                                      self.v_counts,
+                                      self.otu_ids,
+                                      self.tree,
+                                      metric='weighted_unifrac')
+
+        observed_node = self.tree.find_by_id(hotspot_profile['node_address'])
+        self.assertEqual(mocked_return_node, observed_node)
+
+    def test_hotspot_executes_calculate_hotspots(self):
+        mocked_return_node = self.tree
+        with mock.patch('skbio.TreeNode.find_by_id') as mocked_find:
+            mocked_find.return_value = mocked_return_node
+            hotspot_profile = hotspot(self.u_counts,
+                                      self.v_counts,
+                                      self.otu_ids,
+                                      self.tree,
+                                      metric='weighted_unifrac')
+        observed_node = self.tree.find_by_id(hotspot_profile['node_address'])
+        self.assertEqual(mocked_return_node, observed_node)
+
+    def test_hotspot_gets_profile_table_smaller_than_tree(self):
+        with mock.patch('unifrac.gradient._calculate_hotspot') as \
+                mocked_hotspot:
+            mocked_return_node = self.tree
+            mocked_hotspot.return_value = mocked_return_node
+            u_counts = self.u_counts.copy()
+            v_counts = self.v_counts.copy()
+            otu_ids = self.otu_ids.copy()
+            u_counts.pop(), v_counts.pop(), otu_ids.pop()
+            hotspot_profile = hotspot(self.u_counts,
+                                      self.v_counts,
+                                      self.otu_ids,
+                                      self.tree,
+                                      metric='weighted_unifrac')
+
+        observed_node = self.tree.find_by_id(hotspot_profile['node_address'])
+        self.assertEqual(mocked_return_node, observed_node)
+
+    def test_hotspot_gets_profile_works_on_path(self):
+        with mock.patch('unifrac.gradient._calculate_hotspot') as \
+                mocked_hotspot:
+            mocked_return_node = self.tree
+            t1 = self.get_data_path('t1.newick')
+            mocked_hotspot.return_value = mocked_return_node
+            hotspot_profile = hotspot(self.u_counts,
+                                      self.v_counts,
+                                      ['a', 'b', 'c', 'd', 'e'],
+                                      t1,
+                                      metric='weighted_unifrac')
+
+        observed_node = self.tree.find_by_id(hotspot_profile['node_address'])
+        self.assertEqual(mocked_return_node, observed_node)
+
+    def test_hotspot_unsupported_tree_type(self):
+        with self.assertRaisesRegex(ValueError, r'Unsupported type .* for '
+                                                r'tree'):
+            hotspot(self.u_counts, self.v_counts, self.otu_ids,
+                    ['list', 'of', 'things'])
+
+
+class TestEMDUnifrac(unittest.TestCase):
+    package = 'unifrac.tests'
+
+    # see tests in
+    # https://github.com/dkoslicki/EMDUnifrac/blob/master/src/EMDUnifrac.py
+    def test_weighted_emd_unifrac_from_paper(self):
+        tree_str = '((1:0.1,2:0.1)5:0.2,(3:0.1,4:0.1)6:0.2)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['1', '2', '3', '4', '6']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([0, 1 / 2, 1 / 2, 0, 0])
+        sample_2 = np.array([1 / 3, 0, 0, 1 / 3, 1 / 3])
+
+        # expected = beta.weighted_unifrac(sample_1, sample_2, names, tree)
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+
+        # answer from paper
+        expected = 0.2333333
+
+        self.assertAlmostEqual(expected, distance)
+
+    def test_weighted_emd_unifrac_root_diff(self):
+        # TODO check with skbio
+        tree_str = '((1:0.2,2:0.1)5:0.3,(3:0.1,4:0.2)6:0.3)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['1', '2', '3', '4']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([1 / 4, 1 / 4, 1 / 4, 1 / 4])
+        sample_2 = np.array([1 / 2, 1 / 2, 0, 0])
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        print(id_mapping)
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+        # print(diff_abund)
+        self.assertAlmostEqual(0.45, distance)
+
+    def test_weighted_emd_unifrac_bigger_example(self):
+        tree_str = '(((1:0.3, 2:0.3)6:0.5,(3:0.1,4:0.1)7:0.1)8:0.1,5:0.1)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['1', '2', '3', '4', '5']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([0, 0, 1 / 4, 1 / 4, 1 / 2])
+        sample_2 = np.array([1 / 4, 1 / 4, 1 / 4, 1 / 4, 0])
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+
+        id_mapping = {node.id: node.name for
+                      node in tree.postorder()}
+        print(id_mapping)
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+
+        print(diff_abund_names)
+        # print(diff_abund)
+        # multiply by 4 to get counts
+        expected = beta.weighted_unifrac(4 * sample_1, 4 * sample_2, names,
+                                         tree)
+        self.assertAlmostEqual(expected, distance)
+
+    def test_unweighted_emd_unifrac_from_author_implementation(self):
+        # tree_str = '((B:0.1,C:0.2)A:0.3)root;' # author's tree
+        tree_str = '(B:0.1,C:0.2)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['B', 'C']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([1, 1]) / tree.descending_branch_length()
+        sample_2 = np.array([1, 0]) / tree.descending_branch_length()
+        expected = beta.unweighted_unifrac(sample_1, sample_2, names, tree)
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+
+        # distance = distance / tree.descending_branch_length()
+
+        id_mapping = {tree.find_by_id(id_).id: tree.find_by_id(id_).name for
+                      id_ in ids}
+        diff_abund_names = {id_mapping[id_]: abund for id_, abund in
+                            diff_abund.items()}
+        print(diff_abund_names)
+        self.assertAlmostEqual(expected, distance)
+
+    def test_unweighted_emd_unifrac_from_paper(self):
+        tree_str = '((1:0.1,2:0.1)5:0.2,(3:0.1,4:0.1)6:0.2)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        names = ['1', '2', '3', '4']
+        ids = [tree.find(name).id for name in names]
+        sample_1 = np.array([0, 1, 1, 0]) / tree.descending_branch_length()
+        sample_2 = np.array([1, 0, 0, 1]) / tree.descending_branch_length()
+        # get expected from skbio
+        expected = beta.unweighted_unifrac(sample_1, sample_2, names, tree)
+        distance, diff_abund = _emd_unifrac_single_pair(tree,
+                                                        ids,
+                                                        sample_1,
+                                                        sample_2)
+        # distance = distance / tree.descending_branch_length()
+
+        print(diff_abund)
+        print({tree.find_by_id(id_).name: tree.find_by_id(id_) for id_ in ids})
+        print('expected: {}'.format(expected))
+        self.assertAlmostEqual(expected, distance)
+        # TODO check diff_abund
+
+
+class TestWeightAdjuster(TestCase):
+
+    package = 'unifrac.tests'
+
+    def setUp(self):
+        tree_str = '((1:0.2,2:0.1)5:0.3,(3:0.1,4:0.2)6:0.3)root;'
+        tree = TreeNode.read(StringIO(tree_str))
+        tree.assign_ids()
+        self.tree = tree
+        names = ['1', '2', '3', '4']
+        self.ids = [tree.find(name).id for name in names]
+        self.sample_1 = np.array([1, 1, 1, 1])
+        self.sample_2 = np.array([1, 1, 0, 0])
+
+    def test_weighted_adjuster(self):
+        expected_1 = np.array([1/4, 1/4, 1/4, 1/4])
+        expected_2 = np.array([1/2, 1/2, 0, 0])
+        adjusted_s1, adjusted_s2 = _weight_adjuster(self.sample_1,
+                                                    self.sample_2,
+                                                    method='weighted_unifrac')
+
+        self.assertTrue(np.allclose(expected_1, adjusted_s1))
+        self.assertTrue(np.allclose(expected_2, adjusted_s2))
+
+    def test_unweighted_adjuster(self):
+        length = 1.2
+        expected_1 = np.array([1/length, 1/length, 1/length, 1/length])
+        expected_2 = np.array([1/length, 1/length, 0, 0])
+
+        adjusted_s1, adjusted_s2 = _weight_adjuster(self.sample_1,
+                                                    self.sample_2,
+                                                    tree=self.tree,
+                                                    method='unweighted_unifrac')
+
+        self.assertTrue(np.allclose(expected_1, adjusted_s1))
+        self.assertTrue(np.allclose(expected_2, adjusted_s2))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
